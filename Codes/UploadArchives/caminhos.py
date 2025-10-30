9700b6388ed3fa8fb0e63a5f0b3f0eed62cb8665
Pedro <pedrohenriquesslp@gmail.com>
2025-10-30 09:45:29 -0300

    Delete Codes/UploadArchives/caminhos.py

--- Codes/UploadArchives/caminhos.py
+++
@@ -1,3 +0,0 @@
-pasta_raiz = r"C:\Users\pedro.lopes\OneDrive - CAMG\Área de Trabalho\Pasta_Cadmut"
-padrao_arquivo = "*.pdf"
-pasta_temp = r"C:\Users\pedro.lopes\OneDrive - CAMG\Área de Trabalho\Pasta_Destino"
