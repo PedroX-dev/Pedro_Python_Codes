dfb2fb9c814546630fbc258fa1fc750e0cedea24
Pedro <pedrohenriquesslp@gmail.com>
2025-10-30 09:46:38 -0300

    Create caminhos.py

---
+++ Codes/UploadArchive/TheCode/caminhos.py
@@ -0,0 +1,3 @@
+pasta_raiz = r"C:\Users\pedro.lopes\OneDrive - CAMG\Área de Trabalho\Pasta_Cadmut"
+padrao_arquivo = "*.pdf"
+pasta_temp = r"C:\Users\pedro.lopes\OneDrive - CAMG\Área de Trabalho\Pasta_Destino"
