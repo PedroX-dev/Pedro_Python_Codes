5b874ad47cdad6e1487e82083245e9d2b3bee71d
Pedro <pedrohenriquesslp@gmail.com>
2025-11-13 08:44:57 -0300

    Update Save_Credentials.py

--- Codes/Fast_Email/TheCodes/Credentials/Save_Credentials.py
+++ Codes/Fast_Email/TheCodes/Credentials/Save_Credentials.py
@@ -1,11 +1,13 @@
 import keyring
 
-
 servico = "gmail_login"
 usuario = input("Digite seu e-mail do Gmail: ")
 senha = input("Digite sua senha do Gmail: ")
 
-#  Salva no cofre seguro do sistema
-keyring.set_password(servico, usuario, senha)
+def salvar_senha():
+    keyring.set_password(servico, usuario, senha)
+    print(f"Credenciais salvas com sucesso para {usuario}!")
 
-print(f"Credenciais salvas com sucesso para {usuario}!")
+
+if __name__ == "__main__":
+    salvar_senha()
